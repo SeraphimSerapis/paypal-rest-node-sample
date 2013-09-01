dee83a37989fa1b52b9aeb278e18552a8ec8c8e2
Tim Messerschmidt <timmesserschmidt@googlemail.com>
2013-09-01 12:42:55 +0200

    Changes redirects to Heroku

--- routes/index.js
+++ routes/index.js
@@ -28,8 +28,8 @@ exports.create = function (req, res) {
 	if (method === 'paypal') {
 		payment.payer.payment_method = 'paypal';
 		payment.redirect_urls = {
-			"return_url": "http://localhost:" + config.port + "/execute",
-			"cancel_url": "http://localhost:" + config.port + "/cancel"
+			"return_url": "http://pprestnode.herokuapp.com/execute",
+			"cancel_url": "http://pprestnode.herokuapp.com/cancel"
 		};
 	} else if (method === 'credit_card') {
 		var funding_instruments = [
