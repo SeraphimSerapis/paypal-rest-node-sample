e5ace066f393b0684919cbd64083f50a03d162d0
Tarun Maheshwari <ectarun@gmail.com>
2015-03-31 01:24:00 +0530

    Updated with Express V4

--- app.js
+++ app.js
@@ -1,51 +1,52 @@
-'use strict';
-
-/**
- * Module dependencies.
- */
-
 var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 var fs = require('fs');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var errorhandler = require('errorhandler');
+var router = express.Router();
+
 
 var app = express();
-
-// configuration
 try {
-  var configJSON = fs.readFileSync(__dirname + "/config.json");
-  var config = JSON.parse(configJSON.toString());
+    var configJSON = fs.readFileSync(__dirname + "/config.json");
+    var config = JSON.parse(configJSON.toString());
 } catch (e) {
-  console.error("File config.json not found or is invalid: " + e.message);
-  process.exit(1);
+    console.error("File config.json not found or is invalid: " + e.message);
+    process.exit(1);
 }
 routes.init(config);
 
-// all environments
-app.set('port', process.env.PORT || config.port);
+app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'))
+app.use(cookieParser());
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
+//app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler());
+if ('development' == app.get('env')) {
+    app.use(errorhandler());
 }
 
-// routes
 app.get('/', routes.index);
 app.get('/create', routes.create);
 app.get('/execute', routes.execute);
 app.get('/cancel', routes.cancel);
 
-http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
 });
